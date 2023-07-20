f0039ad785f41934ed3c5db27b74502acad74a2a
Injae Lee (이인재) <112389715+llinjae@users.noreply.github.com>
2023-07-20 21:53:35 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,3 @@
+function solution(a, b) {
+    return (a % 2 === 1) && (b % 2 === 1) ? a * a + b * b : (a % 2 === 1) || (b % 2 === 1) ? 2 * (a + b) : (a % 2 === 0) && (b % 2 === 0) ? Math.abs(a - b) : 0;
+}
